659954b4f0014e57a8e34472a1e62f1c5d51e1d4
Kurzov <kurzov.y@gmail.com>
2021-10-01 16:48:20 +0300

    bookshelf effect

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$('.dev_block').on('click', function(){
+		$('.dev_block').removeClass('active');
+		$(this).addClass('active opened');
+		var $this = $(this);
+		setTimeout(function(){
+			$this.siblings('.dev_block').removeClass('opened');
+		}, 100);
+	});
+	$(document).on('click', function (e){
+		var div = $(".dev_block");
+		if (!div.is(e.target) && div.has(e.target).length === 0) {
+			div.removeClass('active');
+			setTimeout(function(){
+				div.removeClass('opened');
+			}, 100);
+		}
+	});
+});	
